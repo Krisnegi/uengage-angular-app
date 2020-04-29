36728f613320031d731ee405c517c1befe0b0a23
Krisnegi <krisnegi12@gmail.com>
2020-04-29 22:33:18 +0530

    updated add product model

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 import { AuthService } from '../auth.service';
 import { products } from '../../data/product';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.css']
 })
 
 export class DashboardComponent implements OnInit {
@@ -17,53 +19,74 @@ export class DashboardComponent implements OnInit {
 
   public productList = [];
   public username = "";
+  addProductForm: FormGroup;
+  isSubmitted = false;
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-     this.productList=products;
-     this.username = localStorage.getItem('USERNAME');
-     this.getProductsCount();
+    this.productList = products;
+    this.username = localStorage.getItem('USERNAME');
+    this.getProductsCount();
+
+    this.addProductForm = this.formBuilder.group({
+      productname: ['', Validators.required],
+      mrp: ['', Validators.required],
+      sp: ['', Validators.required],
+      brand: ['', Validators.required]
+    });
   }
 
-  addProduct(newProductname,newMrp,newSp,newBrand)
-  {
-    const newProduct={productName:newProductname,mrp:newMrp,sp:newSp,brand:newBrand,isActive:true};
+  get formControls() { return this.addProductForm.controls; }
+
+  addProduct() {
+    this.isSubmitted = true;
+
+    if (this.addProductForm.invalid) {
+       return;
+    }
+
+    const formValues = this.addProductForm.value;
+
+    const newProduct = {
+      productName: formValues.productname,
+      mrp: formValues.mrp,
+      sp: formValues.sp,
+      brand: formValues.brand,
+      isActive: true // by default
+    };
+
     this.productList.push(newProduct);
     this.getProductsCount();
   }
 
-  enableProduct(product)
-  {
-    product.isActive=true;
+  enableProduct(product) {
+    product.isActive = true;
     this.getProductsCount();
   }
 
-  disableProduct(product)
-  {
-    product.isActive=false;
+  disableProduct(product) {
+    product.isActive = false;
     this.getProductsCount();
   }
 
-  getProductsCount()
-  {
-    const products=this.productList;
-    let active=0;
-    let inactive=0;
+  getProductsCount() {
+    const products = this.productList;
+    let active = 0;
+    let inactive = 0;
 
-    for(let i=0;i<products.length;i++)
-    {
-      if(products[i].isActive == true)
+    for (let i = 0; i < products.length; i++) {
+      if (products[i].isActive == true)
         active++;
       else
         inactive++;
     }
-    this.totalProducts=products.length;
-    this.activeProducts=active;
-    this.inactiveProducts=inactive;
+    this.totalProducts = products.length;
+    this.activeProducts = active;
+    this.inactiveProducts = inactive;
   }
 
-  logout(){
+  logout() {
     this.authService.logout();
     this.router.navigateByUrl('/login');
   }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,71 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from  '@angular/forms';
-import {Router} from '@angular/router';
-import { AuthService } from  '../auth.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-/*public username="";
-public password="";
-public error_message="";
-  constructor(private route:Router) { }
+  authForm: FormGroup;
+  isSubmitted = false;
+  showInvalidLoginError = false;
+
+  constructor(private authService: AuthService, private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.authService.logout();
+
+    this.authForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
 
-  onClick()
-  {
-    if(this.username!="admin"&&this.password!="admin")
-    {
-       this.error_message="invalid username and password";
-    }
-    else if(this.username!="admin")
-    {
-       this.error_message="invalid username";
-    }
-    else if(this.password!="admin")
-    {
-       this.error_message="invalid password";
-    }
-    else
-    {
-       this.route.navigate(['/dashboard']);
-    }
-  }*/
+  get formControls() { return this.authForm.controls; }
 
-    authForm: FormGroup;
-    isSubmitted  =  false;
-    showInvalidLoginError = false;
-
-    constructor(private authService: AuthService, private router: Router, private formBuilder: FormBuilder ) { }
-
-    ngOnInit() {
-      this.authService.logout();
-
-      this.authForm  =  this.formBuilder.group({
-        username: ['', Validators.required],
-        password: ['', Validators.required]
-      });
+  signIn() {
+    this.isSubmitted = true;
+    if (this.authForm.invalid) {
+      return;
     }
 
-    get formControls() { return this.authForm.controls; }
-
-    signIn(){
-      this.isSubmitted = true;
-      if(this.authForm.invalid){
-        return;
-      }
-
-      if(!this.authService.signIn(this.authForm.value)) {
-        this.showInvalidLoginError = true;
-        return;
-      }
-
-      this.router.navigateByUrl('/dashboard');
+    if (!this.authService.signIn(this.authForm.value)) {
+      this.showInvalidLoginError = true;
+      return;
     }
+
+    this.router.navigateByUrl('/dashboard');
+  }
 }
